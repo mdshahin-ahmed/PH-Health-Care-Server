6c851a178fd77bbc306fd6059b4285cd727ecc8e
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-03-20 21:29:05 +0600

    Add: search functionality

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -5,12 +5,20 @@ import { adminService } from "./admin.service";
 const prisma = new PrismaClient();
 
 const getAllAdminFromDB = async (req: Request, res: Response) => {
-  const result = await adminService.getAllAdminFromDB();
-  res.status(200).json({
-    success: true,
-    message: "Admin data fetched!",
-    data: result,
-  });
+  try {
+    const result = await adminService.getAllAdminFromDB(req.query);
+    res.status(200).json({
+      success: true,
+      message: "Admin data fetched!",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong!",
+      error: err,
+    });
+  }
 };
 
 export const adminController = {
