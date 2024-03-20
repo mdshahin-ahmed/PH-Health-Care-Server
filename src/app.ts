7b2a6819ac479f9570a3fb0f14a92bfd7b1dd1dd
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-03-20 17:32:05 +0600

    Add: user and admin

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { userRoutes } from "./app/modules/User/user";
+import { userRoutes } from "./app/modules/User/user.routes";
 
 const app: Application = express();
 app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from "express";
+import { userController } from "./user.controller";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send({
-    message: "Route is working perfectly!",
-  });
-});
+router.post("/", userController.createAdmin);
 
 export const userRoutes = router;
