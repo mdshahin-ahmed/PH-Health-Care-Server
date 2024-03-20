a0c6973fbdfc9a94d991cf8c7471a36e19fddb20
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-03-20 17:52:39 +0600

    format response and error

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,10 +2,20 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  //   console.log(req.body);
-
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully!",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong!",
+      error: err,
+    });
+  }
 };
 
 export const userController = {
