7df03b5eb70874dc871f8d40b0f3258366e95f1b
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-03-24 12:26:08 +0600

    Add: auth related files

---
+++ src/app/modules/Auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../../shared/catchAsync";
+import { authServices } from "./auth.service";
+import { sendResponse } from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+
+const loginUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = authServices.loginUser(req.body);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Logged in successfully!",
+      data: result,
+    });
+  }
+);
+
+export const authControllers = {
+  loginUser,
+};
