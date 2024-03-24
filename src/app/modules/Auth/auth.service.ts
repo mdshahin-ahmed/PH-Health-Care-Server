ea2387ffaf27c5a94c6efdd559a65ae10f29bd70
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-03-24 16:35:34 +0600

    Add: access token

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,5 +1,32 @@
+import jwt from "jsonwebtoken";
+import { prisma } from "../../../shared/prisma";
+import bcrypt from "bcrypt";
+
 const loginUser = async (payload: { email: string; password: string }) => {
-  console.log(payload);
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+  const isCorrectPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "shahin",
+    {
+      algorithm: "HS256",
+      expiresIn: "15m",
+    }
+  );
+  console.log(accessToken);
+
+  return userData;
 };
 
 export const authServices = {
