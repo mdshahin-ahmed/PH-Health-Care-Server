2a5926cddc6a61ab7d9f34e1851e65104b023a3a
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-03-27 17:57:56 +0600

    Add: doctor model

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -8,7 +8,17 @@ import { userValidation } from "./user.validation";
 const router = express.Router();
 
 router.post(
-  "/",
+  "/create-admin",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data));
+    return userController.createAdmin(req, res, next);
+  }
+);
+
+router.post(
+  "/create-doctor",
   auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
